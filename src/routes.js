652faa0c91e2c156c420f0a1911e39173baea5c0
suttida-105 <suttida.5835512105@gmail.com>
2020-04-11 23:03:49 +0700

    เพิ่มหน้าแสดงรูปทั้งหมด

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import App from './components/App'
 import Upload from './components/Upload'
 import Login from './components/Login'
+import Show from './components/Show'
 
 function Routes () {
     return (
@@ -11,6 +12,7 @@ function Routes () {
                 <Route path='/' exact component={App} />
                 <Route path='/upload' exact component={Upload} />
                 <Route path='/login' exact component={Login} />
+                <Route path='/show' exact component={Show} />
             </Switch>
         </BrowserRouter>
     )
